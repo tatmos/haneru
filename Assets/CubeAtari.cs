83491aa05a5920b5a99442aa631fd1c74854d698
tatmos <charytanaka@gmail.com>
2017-04-26 21:30:22 +0900

    CubeAtari音をつける

---
+++ Assets/CubeAtari.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAtari : MonoBehaviour {
+
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+        Debug.Log("Atari!");
+    }
+}
